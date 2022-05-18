99b2b233ed073199a28f753b40ff5e11088a64da
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-05-18 16:39:45 -0300

    Preparando revisão de código

--- src/routes/signInRoutes.js
+++
@@ -1,7 +0,0 @@
-import postSignIn from '../controllers/signInController.js';
-import { signInValidation } from '../untils/middlewares.js';
-import { Router } from 'express';
-
-const signInRouter = Router();
-signInRouter.post("/signIn", signInValidation,postSignIn);
-export default signInRouter;
