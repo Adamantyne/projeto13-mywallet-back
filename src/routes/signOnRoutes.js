29b81774de2ecafc4fff932318e07de82938bee0
Rodrigodcs <rodrigodcsee@gmail.com>
2022-05-20 12:10:36 -0300

    Preparando revisão de código

--- src/routes/signOnRoutes.js
+++
@@ -1,7 +0,0 @@
-import postSignOn from '../controllers/signOnController.js';
-import { signOnValidation } from '../untils/middlewares.js';
-import { Router } from 'express';
-
-const signOnRouter = Router();
-signOnRouter.post("/signOn", signOnValidation,postSignOn);
-export default signOnRouter;
